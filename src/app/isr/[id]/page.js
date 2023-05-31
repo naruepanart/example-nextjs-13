a26a2d66038e2cb75fc9107d9582407b58a874d8
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-05-31 23:51:46 +0700

    Update page.js

--- src/app/isr/[id]/page.js
+++ src/app/isr/[id]/page.js
@@ -1,3 +1,4 @@
+import PageComponents from "../../../components/PageComponents";
 import axios from "axios";
 import React from "react";
 
@@ -25,10 +26,7 @@ const getPostPage = async ({ params }) => {
 	return (
 		<>
 			<h1>ISR</h1>
-			<p>
-				{data.id} - {data.title}
-			</p>
-			<p>{data.body}</p>
+			<PageComponents key={data.id} posts={data} />
 		</>
 	);
 };
