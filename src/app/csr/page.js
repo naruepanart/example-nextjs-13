49af7f2d953ac2be16f542232532f00f07241cc0
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-06-01 10:07:36 +0700

    ✨feature: add File Conventions

--- src/app/csr/page.js
+++ src/app/csr/page.js
@@ -17,6 +17,7 @@ const page = () => {
 	const fetchData = async () => {
 		const response = await fetch(
 			`https://jsonplaceholder.typicode.com/posts?_page=${page}`,
+			{ next: { revalidate: 3600 } },
 		);
 		const newData = await response.json();
 		setData((prevData) => [...prevData, ...newData]);
@@ -26,6 +27,7 @@ const page = () => {
 	return (
 		<>
 			<h1>CSR</h1>
+
 			{data.map((item) => (
 				<PageComponents key={item.id} posts={item} />
 			))}
