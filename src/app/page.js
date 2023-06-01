52e68e0945415ed01bcfd833884ceb09b86a0d9f
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-06-01 09:47:04 +0700

    🔵 other: move file

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,9 @@
-"use client";
-import PageComponents from "../components/PageComponents";
-import React, { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import React from 'react'
 
 const page = () => {
-	const [data, setData] = useState([]);
-	const [page, setPage] = useState(1);
-	const [ref, inView] = useInView();
+  return (
+	<div>page</div>
+  )
+}
 
-	useEffect(() => {
-		if (inView) {
-			fetchData();
-		}
-	}, [inView]);
-
-	const fetchData = async () => {
-		const response = await fetch(
-			`https://jsonplaceholder.typicode.com/posts?_page=${page}`,
-		);
-		const newData = await response.json();
-		setData((prevData) => [...prevData, ...newData]);
-		setPage((prevPage) => prevPage + 1);
-	};
-
-	return (
-		<>
-			<h1>Home</h1>
-			{data.map((item) => (
-				<PageComponents key={item.id} posts={item} />
-			))}
-			<div ref={ref} />
-		</>
-	);
-};
-
-export default page;
+export default page
